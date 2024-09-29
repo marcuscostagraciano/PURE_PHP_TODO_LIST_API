6903be196f407df92b9099826bd2f858f385a38c
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-09-28 21:48:07 -0300

    FEAT: add a way see the id to consult via url

--- index.php
+++ index.php
@@ -5,11 +5,13 @@ require 'src/autoload.php';
 $request_uri = explode('/', $_SERVER['REQUEST_URI']);
 // 0 is an empty string. 1 is the folder name. 2 is the endpoint
 $request_endpoint = $request_uri[2];
+$id_to_consult = $request_uri[3] ?? null;
 $request_method = $_SERVER['REQUEST_METHOD'];
 
 $request_info = [
     'METHOD' => $request_method,
     'ENDPOINT' => $request_endpoint,
+    'IDTOCONSULT' => $id_to_consult,
     'PARAMS' => $_REQUEST,
     'BODY' => json_decode(file_get_contents('php://input')),
 ];
