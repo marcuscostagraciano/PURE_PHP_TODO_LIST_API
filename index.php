6747b9d190a25516d6b14fa7380eb16012a12251
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-09-28 22:12:22 -0300

    FEAT: remove the 'endpoint' and add new way to get it and the 'id to consult'

--- index.php
+++ index.php
@@ -3,17 +3,16 @@ require 'src/autoload.php';
 
 
 $request_uri = explode('/', $_SERVER['REQUEST_URI']);
-// 0 is an empty string. 1 is the folder name. 2 is the endpoint
-$request_endpoint = $request_uri[2];
-$id_to_consult = $request_uri[3] ?? null;
+$request_endpoint = prev($request_uri);
+$id_to_consult = end($request_uri);
 $request_method = $_SERVER['REQUEST_METHOD'];
 
 $request_info = [
     'METHOD' => $request_method,
-    'ENDPOINT' => $request_endpoint,
-    'IDTOCONSULT' => $id_to_consult,
+    // 'ENDPOINT' => $request_endpoint,
+    'ID_TO_CONSULT' => $id_to_consult,
     'PARAMS' => $_REQUEST,
-    'BODY' => json_decode(file_get_contents('php://input')),
+    'BODY' => json_decode(file_get_contents('php://input'), true),
 ];
 
 $request_handler = new RequestHandler($request_info);
