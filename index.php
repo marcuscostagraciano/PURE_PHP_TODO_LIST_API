2f0108c4bffba05e6408cbe1d1910d3e00d1be34
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-10-05 11:34:19 -0300

    FEAT: add response to preflight requests

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require 'src/autoload.php';
 
-
 $request_uri = explode('/', $_SERVER['REQUEST_URI']);
 $request_endpoint = prev($request_uri);
 $id_to_consult = end($request_uri);
@@ -14,6 +13,15 @@ $request_info = [
     'PARAMS' => $_REQUEST,
     'BODY' => json_decode(file_get_contents('php://input'), true),
 ];
+
+if ($request_method == 'OPTIONS') {
+    $CACHED_MINUTES_OF_PREFLIGHT_REQUEST = 60;
+
+    http_response_code(200);
+    header('Access-Control-Allow-Methods: DELETE, GET, PATCH, POST');
+    header("Access-Control-Max-Age: 60 * $CACHED_MINUTES_OF_PREFLIGHT_REQUEST");
+    die();
+}
 
 $request_handler = new RequestHandler($request_info);
 $data = $request_handler->getResponse();
